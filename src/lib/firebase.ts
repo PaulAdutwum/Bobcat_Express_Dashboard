ec8110bc2e4d11e1edfb2d69fcd519e6873f015b
Paul Adutwum <pauladutwum303@gmail.com>
2025-02-21 04:26:24 -0500

    Upadated Firebase api setup

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,6 +12,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+
 };
 
 // 📌 Initialize Firebase App (Avoid Duplicate Instances)
